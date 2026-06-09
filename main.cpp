37e7ce3897ed77ab906643add0017a9fa116bcf0
urmom789 <nathankeele01@gmail.com>
2026-06-09 05:32:03 +0000

    Jun 8, 2026, 10:32 PM

--- main.cpp
+++ main.cpp
@@ -139,7 +139,9 @@ int main()
   cout << "First 5 regions after sorting by ID:" << endl;
 
   for (int i = 0; i < 5 && i < regions.size(); i++) {
-    cout regions[i].id << " | " << regions[i].city << " | " << regions[i].state
-                       << endl;
+    cout << regions[i].id << " | " << regions[i].city << " | "
+         << regions[i].state << endl;
   }
+
+  cout << "-----------------------------Part 3----------------------------\n";
 }
