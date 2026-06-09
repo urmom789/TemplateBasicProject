d09dc13ce340b0e648d04d979d8f9f02fb551924
urmom789 <nathankeele01@gmail.com>
2026-06-09 05:28:28 +0000

    Jun 8, 2026, 10:28 PM

--- main.cpp
+++ main.cpp
@@ -122,8 +122,8 @@ int main()
   regionFile.close();
 
   cout << "First 5 regions:" << endl;
-  for (int i = 0; i < 5 && i < regions.size(); i++ {
-         cout regions[i].id << " | " << regions[i].city << " | "
-                            << regions[i].state << endl;
-       })
+  for (int i = 0; i < 5 && i < regions.size(); i++) {
+    cout << regions[i].id << " | " << regions[i].city << " | "
+         << regions[i].state << endl;
+  }
 }
